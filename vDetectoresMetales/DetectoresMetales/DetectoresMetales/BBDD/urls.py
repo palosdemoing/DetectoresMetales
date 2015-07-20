cfbfa6dbc937f3059959ecd50a598a3a0eb21917
palosdemoing <pal.ocariz@gmail.com>
2015-07-20 21:16:11 +0200

    Error con inserción por autonumérico de de la PK
    arreglado ejecutando en MySQL:
    	ALTER TABLE `table_name`  AUTO_INCREMENT = 1
    
    los otros cambios, en nombres de variables de cara al futuro

--- vDetectoresMetales/DetectoresMetales/DetectoresMetales/BBDD/urls.py
+++ vDetectoresMetales/DetectoresMetales/DetectoresMetales/BBDD/urls.py
@@ -15,6 +15,14 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('DetectoresMetales.BBDD.views',
     url(r'^$', 'expedientes_list', name='expedientes_list'),
-    url(r'^BBDD/add/$', 'expedientes_add', name='expedientes_add'),
-    url(r'^BBDD/edit/(?P<expedienteitem_pk>\d+)/$', 'expedientes_edit', name='expedientes_edit'),
+    url(r'^BBDD/expedientes_add/$', 'expedientes_add', name='expedientes_add'),
+    url(r'^BBDD/expedientes_edit/(?P<expedienteitem_pk>\d+)/$', 'expedientes_edit', name='expedientes_edit'),
+            # parametro para que servidor la identifique
+            
+                # en pythex ->    ^BBDD/edit/(?P<newsitem_pk>\d+)/$
+                # esta match con     BBDD/edit/3/
+                
+    # si cambio aqui las URL no interfiere con las llamadas desde templatetags, 
+    # que van directamente a las funciones de las views.py
+    
 )
